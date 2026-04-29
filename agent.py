68e42f17fbc262499e5254a60df91cfc7dc3bcd4
meerab00 <sandhuchoudry@gmail.com>
2026-04-29 16:49:48 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,16 +1,20 @@
-from recommender import get_recommendations
+
+from recommender import recommend_items
 
 def run_agent(user_input, df):
+
     user_input = user_input.lower()
 
-    # simple intent detection
+    # Intent handling (simple AI brain)
     if "recommend" in user_input or "suggest" in user_input:
-        result = get_recommendations(df)
-        return f"Here are your recommendations:\n\n{result}"
+
+        result = recommend_items(df, user_input)
+
+        return f"Here are smart recommendations based on your data:\n\n{result}"
 
     elif "sad" in user_input:
-        result = get_recommendations(df)
-        return f"For sad mood, I suggest:\n\n{result}"
+        result = recommend_items(df, user_input)
+        return f"For sad mood, I found these:\n\n{result}"
 
     else:
-        return "Please ask for recommendations like 'suggest me products or movies'"
+        return "Tell me like: recommend me products or movies 😊"
